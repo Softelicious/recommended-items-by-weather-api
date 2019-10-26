28eef9e28278471286086c5aff349893e750403e
Softelicious <dominykas.rakunas@gmail.com>
2019-10-27 02:14:20 +0300

    Test written

--- server/tests/Feature/ExampleTest.php
+++ server/tests/Feature/ExampleTest.php
@@ -12,10 +12,21 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    public function testBasicTest()
+    public function testIfEndpointWorks()
     {
-        $response = $this->get('/');
-
+        $response = $this->get('/api/products/recommended/Kaunas');
         $response->assertStatus(200);
     }
+
+    public function testIfEndointRetursCity()
+    {
+        $input = 'kaunas';
+        $response = $this->get("/api/products/recommended/$input");
+
+        $response->assertJson([
+            'city' => $input,
+        ]);
+    }
+
+
 }
--- server/tests/Unit/ExampleTest.php
+++ server/tests/Unit/ExampleTest.php
@@ -2,6 +2,11 @@
 
 namespace Tests\Unit;
 
+use App\Http\Resources\ProductsResource;
+use App\Product;
+use App\Weather;
+use GuzzleHttp\Client;
+use SebastianBergmann\Environment\Console;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -12,8 +17,56 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    public function testBasicTest()
+    public function testCheckIfValidationPasses()
     {
-        $this->assertTrue(true);
+            $input = 'kaunas';
+            $client = new Client();
+            try{
+                $response = $client->get("https://api.meteo.lt/v1/places/$input/forecasts/long-term")->getBody();
+            }catch(\Exception $e){
+                $this->assertFalse(true);
+                return;
+            }
+            $this->assertTrue(true);
+            return;
+    }
+
+    public function testCheckIfValidationDoesntPass()
+    {
+        $input = 'kauna';
+        $client = new Client();
+        $response = json_encode([]);
+        try{
+            $response = $client->get("https://api.meteo.lt/v1/places/$input/forecasts/long-term")->getBody();
+        }catch(\Exception $e){
+            $array = json_decode($response);
+            $this->assertTrue(true);
+            return;
+        }
+        $this->assertFalse(true);
+        return;
+    }
+    public function testCheckIfValidationDoesntPass2()
+    {
+        $input = '2';
+        $client = new Client();
+        $response = json_encode([]);
+        try{
+            $response = $client->get("https://api.meteo.lt/v1/places/$input/forecasts/long-term")->getBody();
+        }catch(\Exception $e){
+            $array = json_decode($response);
+            $this->assertTrue(true);
+            return;
+        }
+        $this->assertFalse(true);
+        return;
+    }
+    public function testCheckIfSeedsWorked(){
+        $data = Product::find(15)->whethers;
+        if(count($data)>0){
+            $this->assertTrue(true);
+        }else{
+            $this->assertFalse(true);
+        }
     }
 }
